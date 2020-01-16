1ccce1f8bc60aeae3a040436dd2220cd6d074d36
Denis Onder <denis_onder@protonmail.com>
2020-01-16 20:47:51 +0100

    Setting 0th element to be 100% wide if it's the only child

--- main.js
+++ main.js
@@ -7,8 +7,12 @@ function logChildren(line) {
   // NOTE Logging children for testing
   const bars = Array.from(line.children);
   // Generate a random number to represent the width of each bar.
-  const number = Math.random() * 100;
-  // Set the first bar's width to the number, and set the second bar's width to 100% - the number.
-  bars[0].style.width = `${number}%`;
-  bars[1].style.width = `${100 - number}%`;
+  if (bars.length === 1) {
+    bars[0].style.width = "100%";
+  } else {
+    const number = Math.random() * 100;
+    // Set the first bar's width to the number, and set the second bar's width to 100% - the number.
+    bars[0].style.width = `${number}%`;
+    bars[1].style.width = `${100 - number}%`;
+  }
 }
